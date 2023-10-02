69d16e64daa45ea816c9a242bb4183e920966103
Kavita Pathak <110056559+Kunalpathak2004@users.noreply.github.com>
2023-10-02 12:31:14 +0530

    incremented the count

--- app.js
+++ app.js
@@ -3,10 +3,16 @@
 var count = 0;
 
 var countEl = document.getElementById("count-el");
-console.log(countEl);
+//console.log(countEl);
 
 function increment() {
      count = count + 1;
      console.log(count);
      countEl.innerText = count;   
+}
+
+function save() {
+     count = count + 1;
+     console.log(count);
+     document.getElementById("save-btn").innerText = count;
 }
